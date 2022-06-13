57673a7b5345d5083a0155d39c9d5bc21bc8d783
Talip <talipakcelik@gmail.com>
2022-06-13 16:58:24 +0300

    add map over API data, for accessing answers properly

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from "react";
 import Quiz from "./Quiz";
+import { nanoid } from "nanoid";
+
 export default function App() {
   const [start, setStart] = React.useState(false);
-  const [questions, setQuestions] = React.useState();
+  const [quiz, setQuiz] = React.useState();
 
-  let questionElements;
+  let quizElements;
 
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&type=multiple").then((res) =>
-      res.json().then((data) => setQuestions(data.results))
+      res.json().then((data) => {
+        const newArr = data.results.map((el) => ({
+          question: el.question,
+          answers: [...el.incorrect_answers, el.correct_answer].map((el) => ({
+            answer: el,
+            id: nanoid(),
+            isSelected: false,
+          })),
+        }));
+
+        return setQuiz(newArr);
+      })
     );
   }, []);
 
-  if (typeof questions === "object") {
-    questionElements = questions.map((question, index) => (
-      <Quiz key={index} questions={question} />
+  function toggle(id) {
+    console.log(id);
+  }
+
+  if (typeof quiz === "object") {
+    quizElements = quiz.map((item, index) => (
+      <Quiz key={index} questions={item} tog={toggle} />
     ));
-    console.log(questions);
   }
 
   return (
@@ -27,7 +43,7 @@ export default function App() {
           <button onClick={() => setStart(true)}>Start quiz</button>
         </div>
       ) : (
-        questionElements
+        quizElements
       )}
     </main>
   );
