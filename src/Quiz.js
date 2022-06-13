1b7d098f13f89331ce9f2c9817bcfc544c909d74
Talip <talipakcelik@gmail.com>
2022-06-13 21:36:40 +0300

    refactor props name

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,21 +10,46 @@ export default function Quiz(props) {
 
   console.log(props);
 
+  const q = {};
   return (
-    <div>
-      <h2>{props.questions.question}</h2>
-      <button onClick={() => props.tog(props.questions.answers[0].id)}>
-        {props.questions.answers[0].answer}
-      </button>
-      <button onClick={() => props.tog(props.questions.answers[1].id)}>
-        {props.questions.answers[1].answer}
-      </button>
-      <button onClick={() => props.tog(props.questions.answers[2].id)}>
-        {props.questions.answers[2].answer}
-      </button>
-      <button onClick={() => props.tog(props.questions.answers[3].id)}>
-        {props.questions.answers[3].answer}
-      </button>
+    <div className="quiz">
+      <h2>{props.questions}</h2>
+      <div
+        className="answer"
+        style={{
+          backgroundColor: props.answers[0].isSelected ? "#D6DBF5" : "",
+        }}
+        onClick={() => props.tog(props.answers[0].id)}
+      >
+        {props.answers[0].answer}
+      </div>
+      <div
+        className="answer"
+        style={{
+          backgroundColor: props.answers[1].isSelected ? "#D6DBF5" : "",
+        }}
+        onClick={() => props.tog(props.answers[1].id)}
+      >
+        {props.answers[1].answer}
+      </div>
+      <div
+        className="answer"
+        style={{
+          backgroundColor: props.answers[2].isSelected ? "#D6DBF5" : "",
+        }}
+        onClick={() => props.tog(props.answers[2].id)}
+      >
+        {props.answers[2].answer}
+      </div>
+      <div
+        className="answer"
+        style={{
+          backgroundColor: props.answers[3].isSelected ? "#D6DBF5" : "",
+        }}
+        onClick={() => props.tog(props.answers[3].id)}
+      >
+        {props.answers[3].answer}
+      </div>
       <hr></hr>
     </div>
   );
